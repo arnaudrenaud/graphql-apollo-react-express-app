12be8cdb37e27261622322ed7d7562fda226d8ba
Arnaud Renaud <arnaud.renaud@gmail.com>
2019-02-26 11:49:15 +0100

    Remove unused REST API code

--- api/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-import comments from './comments';
-import items from './items';
-
-export default Router()
-  .use('/items', items)
-  .use('/comments', comments);
